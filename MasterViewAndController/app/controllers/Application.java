c13f03bd454e7f51ec16b149e437e60bb7e81be5
hiroo takaku <slgerkamp@gmail.com>
2014-04-05 15:56:05 +0900

    3.1.7 routesを使った引数の指定

--- MasterViewAndController/app/controllers/Application.java
+++ MasterViewAndController/app/controllers/Application.java
@@ -7,28 +7,30 @@ import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
+	// routesで指定したindex/:msgにより、
+	// index/パラメータという形でアクセスが可能に
+	// パラメータが取得可能になる
+	// 複数指定する場合はindex/:msg~:idのように
+	public static Result index(String msg, int id ) {
 
-    	// Http.Request：リクエストを管理するクラス
-    	String msg = request().getQueryString("msg");
-        return ok(index.render("引数は：" + msg + "です"));
+        return ok(index.render("引数は：" + msg + ", " + id + "です"));
     }
 
 
-    public static Result redirect() {
-
-    	// トップページにリダイレクト
-    	// アドレスを静的に記述していると変更が面倒
-    	// return redirect("/");
-    	// 
-    	// リバースルーティングという手法を使う
-    	// これにより、アドレス変更の影響を受けない
-    	// 
-    	// routesファイルに記述されたControllerのルーティング情報は、
-    	// controllers.routesにまとめられる
-    	// controllers.routes.ApplicationはControllers.Applicationとは異なる
-    	// 「ReverseApplication」というクラスで、Application.index()に割り当てられたアドレスがわかる
-    	return redirect(controllers.routes.Application.index());
-    }
+//    public static Result redirect() {
+//
+//    	// トップページにリダイレクト
+//    	// アドレスを静的に記述していると変更が面倒
+//    	// return redirect("/");
+//    	// 
+//    	// リバースルーティングという手法を使う
+//    	// これにより、アドレス変更の影響を受けない
+//    	// 
+//    	// routesファイルに記述されたControllerのルーティング情報は、
+//    	// controllers.routesにまとめられる
+//    	// controllers.routes.ApplicationはControllers.Applicationとは異なる
+//    	// 「ReverseApplication」というクラスで、Application.index()に割り当てられたアドレスがわかる
+//    	return redirect(controllers.routes.Application.index());
+//    }
 
 }
