bffe2691f23fbac1af4b7073527b5be10c7afc74
hiroo takaku <slgerkamp@gmail.com>
2014-04-05 15:23:32 +0900

    3.1.4 index.scala.htmlだけで表示

--- MasterViewAndController/app/controllers/Application.java
+++ MasterViewAndController/app/controllers/Application.java
@@ -10,10 +10,8 @@ public class Application extends Controller {
     public static Result index() {
 
     	// okメソッドは文字列をResultインスタンにして返却する
-    	// HTMLで出力するにはasメソッドを使って、形式を明確にする
-    	// 記述形式はHTMLのContent-typeを設定する
-        return ok("<html><body><h1>Hello!</h1><p>This is Test.</p></body></html>")
-        		.as("text/html");
+    	// index.scala.htmlをレンダリングしている
+        return ok(index.render("これはテンプレートのテストです"));
     }
 
 }
