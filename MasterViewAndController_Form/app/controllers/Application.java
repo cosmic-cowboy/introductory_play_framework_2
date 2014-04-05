d1566a089a3c718cd8020662b53480b9aaa19e53
hiroo takaku <slgerkamp@gmail.com>
2014-04-05 17:14:50 +0900

    3.1.8 フォームの送信

--- MasterViewAndController_Form/app/controllers/Application.java
+++ MasterViewAndController_Form/app/controllers/Application.java
@@ -1,14 +1,27 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import java.util.Map;
 
-import views.html.*;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
 
 public class Application extends Controller {
 
+	
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+    	// メソッド判定
+    	String method = request().method();
+    	
+    	if("GET".equals(method)){
+            return ok(index.render("please type:"));
+    	} else {
+    		// Http.Request > Http.RequestBody > Form情報
+    		Map<String, String[]> form = request().body().asFormUrlEncoded();
+    		// Form情報でname=inputの値を取得する
+    		String [] input = form.get("input");
+            return ok(index.render("posted:" + input[0]));
+    	}
     }
 
 }
