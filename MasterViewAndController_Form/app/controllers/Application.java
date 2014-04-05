4971c2282f0dace3c164d322139b9bad1b49a9c6
hiroo takaku <slgerkamp@gmail.com>
2014-04-05 21:59:52 +0900

    3.2 フォームヘルパーの利用

--- MasterViewAndController_Form/app/controllers/Application.java
+++ MasterViewAndController_Form/app/controllers/Application.java
@@ -1,27 +1,59 @@
 package controllers;
 
+import java.util.Date;
 import java.util.Map;
 
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
 
 public class Application extends Controller {
 
+	public static class SampleForm {
+		public String input;
+		public String pass;
+		public boolean check;
+		public String radio;
+		public String sel;
+		public String area;
+		public Date date;
+	}
+	
 	
     public static Result index() {
-    	// メソッド判定
-    	String method = request().method();
     	
-    	if("GET".equals(method)){
-            return ok(index.render("please type:"));
-    	} else {
-    		// Http.Request > Http.RequestBody > Form情報
-    		Map<String, String[]> form = request().body().asFormUrlEncoded();
-    		// Form情報でname=inputの値を取得する
-    		String [] input = form.get("input");
-            return ok(index.render("posted:" + input[0]));
-    	}
+    	// 初期値を設定する
+    	SampleForm sf = new SampleForm();
+    	sf.radio = "windows";
+    	sf.check = true;
+    	sf.input = "default value";
+    	sf.sel = "uk";
+    	Form<SampleForm> form = new Form(SampleForm.class).fill(sf);
+    	// 設定された初期値をフォームに代入する
+		return ok(index.render("please set form.", form));
+    	
     }
 
+    public static Result send() {
+    	// フォームに代入された値を取得する
+    	Form<SampleForm> f = new Form(SampleForm.class).bindFromRequest();
+    	
+    	if (!f.hasErrors()){
+    		// フォームに代入された値を表示する
+    		SampleForm sf = f.get();
+    		String res = "value: ";
+    		res += "input = " + sf.input
+    				+ ", pass = "  + sf.pass
+    				+ ", check = " + sf.check
+    				+ ", radio = " + sf.radio
+    				+ ", sel = "   + sf.sel
+    				+ ", area = "  + sf.area
+    				+ ", date = "  + sf.date;
+    		return ok(index.render(res, f));
+    	} else {
+    		return badRequest(index.render("ERROR",f));
+    	}
+    	
+    }
 }
