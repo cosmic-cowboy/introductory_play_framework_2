ba69af3f64c0e7ff13fb2c864365dfaf28ec3f1a
hiroo takaku <slgerkamp@gmail.com>
2014-04-13 14:38:25 +0900

    5.1.3 Modelに必要なふるまい

---
+++ checkYou/app/models/services/Check/CheckService.java
@@ -0,0 +1,15 @@
+package models.services.Check;
+
+import play.libs.F.Option;
+
+public class CheckService {
+	
+    public Option<String> getResult(String name) {
+    	return null;
+    }
+    
+    public Option<String> getResultText(String name, String version) {
+    	return null;
+    }
+
+}
