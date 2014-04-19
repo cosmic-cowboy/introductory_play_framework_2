8abd26c5d9d2c0800f405686f754904bfb3edfb2
hiroo takaku <slgerkamp@gmail.com>
2014-04-19 17:36:41 +0900

    5.2.3 Helperの活用方法その２：表示と日付変換の役割を明確に

--- checkYou/app/controllers/Application.java
+++ checkYou/app/controllers/Application.java
@@ -1,14 +1,15 @@
 package controllers;
 
+import java.util.Date;
+
 import play.*;
 import play.mvc.*;
-
 import views.html.*;
 
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render("Your new application is ready.", new Date()));
     }
 
 }
