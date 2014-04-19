d10cfeac17e889958b5b02225f3c8a1b390a96ab
hiroo takaku <slgerkamp@gmail.com>
2014-04-19 15:09:44 +0900

    5.1.9 診断を行う

--- checkYou/app/models/services/Check/CheckService.java
+++ checkYou/app/models/services/Check/CheckService.java
@@ -1,15 +1,43 @@
 package models.services.Check;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import utils.ConfigUtil;
+import utils.OptionUtil;
 import play.libs.F.Option;
 
 public class CheckService {
 	
+    /**
+     * 診断を行う
+     * @param name
+     * @return
+     */
     public Option<String> getResult(String name) {
-    	return null;
+    	@SuppressWarnings("serial")
+		List<String> defaultList = new ArrayList<String>(){{add("2.1.3 Java");}};
+    	List<String> versions = 
+    			ConfigUtil.getByList("checkyou.setting.answer")
+    			.getOrElse(defaultList);
+    	Collections.shuffle(versions);
+    	return getResultText(name, versions.get(0));
     }
     
+    /**
+     * 診断結果
+     * @param name
+     * @param version
+     * @return
+     */
     public Option<String> getResultText(String name, String version) {
-    	return null;
+    	StringBuilder result = new StringBuilder();
+    	result.append(name);
+    	result.append(ConfigUtil.get("checkyou.setting.message.result").getOrElse("."));
+    	result.append(version);
+    	result.append(ConfigUtil.get("checkyou.setting.message.resultSuffix").getOrElse("."));
+    	return OptionUtil.apply(result.toString());
     }
 
 }
