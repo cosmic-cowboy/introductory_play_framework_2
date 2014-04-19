c40830fb0971fcd31094df799168d64eff641edf
hiroo takaku <slgerkamp@gmail.com>
2014-04-19 17:57:53 +0900

    5.2.4 診断アプリに必要なviewを設定

--- checkYou/app/controllers/Application.java
+++ checkYou/app/controllers/Application.java
@@ -9,7 +9,24 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready.", new Date()));
+        return ok(views.html.check.index.render("Your new application is ready.", new Date()));
     }
 
+
+    public static Result result() {
+        return ok(views.html.check.result.render("Your new application is ready.", new Date()));
+    }
+
+
+    public static Result resultId(Long id) {
+        return ok(views.html.check.result.render("Your new application is ready.", new Date()));
+    }
+
+
+    public static Result recent(Integer page) {
+        return ok(views.html.check.recent.render("Your new application is ready.", new Date()));
+    }
+
+
+    
 }
