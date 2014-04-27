687a8f5639fe307f50dd85524fcf04cc940a2c01
hiroo takaku <slgerkamp@gmail.com>
2014-04-27 23:16:06 +0900

    6.2.3 APIのインタフェース

---
+++ checkYou/app/controllers/api/Checks.java
@@ -0,0 +1,18 @@
+package controllers.api;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+/**
+ * API用Controller
+ */
+public class Checks extends Controller {
+
+    public static Result check(Long id){
+    	return TODO;
+    }
+
+    public static Result getList(Integer page){
+    	return TODO;
+    }
+}
