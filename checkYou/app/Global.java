4f2e39dc19d7534a861b0680b577dfacc767b45f
hiroo takaku <slgerkamp@gmail.com>
2014-04-27 17:02:17 +0900

    6.1.2 リクエスト単位のログ

--- checkYou/app/Global.java
+++ checkYou/app/Global.java
@@ -1,6 +1,12 @@
+import java.lang.reflect.Method;
+
 import play.GlobalSettings;
+import play.Logger;
 import play.api.mvc.EssentialFilter;
 import play.filters.csrf.CSRFFilter;
+import play.libs.Json;
+import play.mvc.Action;
+import play.mvc.Http.Request;
 
 
 public class Global extends GlobalSettings{
@@ -14,5 +20,11 @@ public class Global extends GlobalSettings{
 		return new Class[]{
 				CSRFFilter.class
 		};
+	}
+	
+	@Override
+	public Action onRequest(Request request, Method method) {
+		Logger.info(Json.toJson(request.headers()).toString());
+		return super.onRequest(request, method);
 	}
 }
