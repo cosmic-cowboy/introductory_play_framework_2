a867113bf080a28252aef58e9ea5f34f7ca9ab59
hiroo takaku <slgerkamp@gmail.com>
2014-04-29 19:38:04 +0900

    7.1.8 Ebeanモデルまわりのユニットテスト

--- checkYou/test/models/service/Check/CheckModelServiceTest.java
+++ checkYou/test/models/service/Check/CheckModelServiceTest.java
@@ -1,5 +1,28 @@
 package models.service.Check;
 
-public class CheckModelServiceTest {
+import org.junit.Test;
 
+import models.entity.Check;
+import models.services.Check.CheckModelService;
+import play.libs.F.Option;
+import play.libs.F.Some;
+import static org.fest.assertions.Assertions.assertThat;
+
+import com.avaje.ebean.Ebean;
+
+import apps.FakeApp;
+
+public class CheckModelServiceTest extends FakeApp {
+
+	@Test
+	public void テーブルから1件のレコードから1つ取り出す(){
+		Ebean.execute(Ebean.createSqlUpdate(
+				"INSERT INTO `checks` (`id`, `name`, `result`, `result`, `created`, `modified`) VALUES ('1',  'kara_d',  'kara_dさんにオススメなPlay frameworkのバージョンは、2.1.3 Javaです。',  '2013-08-20 12:34:56', '2013-08-20 12:34:56');"
+		));
+		Option<Check> model = new CheckModelService().findById(1L);
+		assertThat(model.getClass()).isEqualTo(Some.class);
+		assertThat(model.get().getId()).isEqualTo(1L);
+		assertThat(model.get().getName()).isEqualTo("kara_d");
+		assertThat(model.get().getResult()).isEqualTo("kara_dさんにオススメなPlay frameworkのバージョンは、2.1.3 Javaです。");
+	}
 }
