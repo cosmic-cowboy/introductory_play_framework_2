7e69f09f48daf67d81824a5cdd9cf16152d0872e
hiroo takaku <slgerkamp@gmail.com>
2014-04-29 17:46:10 +0900

    7.1.6 ユーティリティクラスのユニットテスト

--- checkYou/test/utils/OptionUtilTest.java
+++ checkYou/test/utils/OptionUtilTest.java
@@ -1,5 +1,82 @@
 package utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import play.libs.F;
+import static org.fest.assertions.Assertions.assertThat;
+
 public class OptionUtilTest {
 
+	@Test
+	public void 有効値を入力(){
+		String test = "abc";
+		F.Option<String> strOps = OptionUtil.apply(test);
+		assertThat(strOps.getClass()).isEqualTo(F.Some.class);
+		assertThat(strOps.getClass()).isNotEqualTo(F.None.class);
+	}
+	
+	@Test
+	public void NULLを入力(){
+		String test = null;
+		F.Option<String> strOps = OptionUtil.apply(test);
+		assertThat(strOps.getClass()).isNotEqualTo(F.Some.class);
+		assertThat(strOps.getClass()).isEqualTo(F.None.class);
+	}
+
+	@Test
+	public void 有効なListを入力(){
+		List<String> testList = new ArrayList<String>();
+		testList.add("a");
+		testList.add("b");
+		testList.add("c");
+		F.Option<List<String>> strOps = OptionUtil.apply(testList);
+		assertThat(strOps.getClass()).isEqualTo(F.Some.class);
+		assertThat(strOps.getClass()).isNotEqualTo(F.None.class);
+	}
+	
+	@Test
+	public void 空のListを入力(){
+		List<String> testList = new ArrayList<String>();
+		F.Option<List<String>> strOps = OptionUtil.apply(testList);
+		assertThat(strOps.getClass()).isNotEqualTo(F.Some.class);
+		assertThat(strOps.getClass()).isEqualTo(F.None.class);
+	}
+	
+	@Test
+	public void 有効な文字列を入力(){
+		String test = "abc";
+		F.Option<String> strOps = OptionUtil.applyWithString(test);
+		assertThat(strOps.getClass()).isEqualTo(F.Some.class);
+		assertThat(strOps.getClass()).isNotEqualTo(F.None.class);
+	}
+	
+	@Test
+	public void 空文字を入力(){
+		String test = "";
+		F.Option<String> strOps = OptionUtil.applyWithString(test);
+		assertThat(strOps.getClass()).isNotEqualTo(F.Some.class);
+		assertThat(strOps.getClass()).isEqualTo(F.None.class);
+	}
+	
+	@Test
+	public void 有効値をScalaに変換(){
+		String test = "abc";
+		F.Option<String> strOps = OptionUtil.apply(test);
+		scala.Option<String> strOptScala = OptionUtil.asScala(strOps);
+		assertThat(strOps.getClass()).isEqualTo(F.Some.class);
+		assertThat(strOptScala.getClass()).isEqualTo(scala.Some.class);
+	}
+	
+	@Test
+	public void 無効値をScalaに変換(){
+		String test = null;
+		F.Option<String> strOps = OptionUtil.apply(test);
+		scala.Option<String> strOptScala = OptionUtil.asScala(strOps);
+		assertThat(strOps.getClass()).isEqualTo(F.None.class);
+		assertThat(strOptScala.getClass()).isEqualTo(scala.None$.class);
+	}
+	
 }
--- checkYou/test/utils/PageUtilTest.java
+++ checkYou/test/utils/PageUtilTest.java
@@ -1,5 +1,62 @@
 package utils;
 
+import org.junit.Test;
+import static org.fest.assertions.Assertions.assertThat;
+
 public class PageUtilTest {
+
+	/**
+	 * ページ数が 0 のとき
+	 * @throws Exception
+	 */
+	@Test
+	public void ページ数が0() throws Exception {
+		assertThat(PageUtil.rightPage(0)).isEqualTo(0);
+	}
+	
+	/**
+	 * ページ数が -1 のとき
+	 * @throws Exception
+	 */
+	@Test
+	public void ページ数がマイナス1() throws Exception {
+		assertThat(PageUtil.rightPage(-1)).isEqualTo(0);
+	}
+
+	/**
+	 * ページ数が 1 のとき
+	 * @throws Exception
+	 */
+	@Test
+	public void ページ数が1() throws Exception {
+		assertThat(PageUtil.rightPage(1)).isEqualTo(0);
+	}
+
+	/**
+	 * ページ数が 2 のとき
+	 * @throws Exception
+	 */
+	@Test
+	public void ページ数が2() throws Exception {
+		assertThat(PageUtil.rightPage(2)).isEqualTo(1);
+	}
+
+	/**
+	 * ページ数が 10 のとき
+	 * @throws Exception
+	 */
+	@Test
+	public void ページ数が10() throws Exception {
+		assertThat(PageUtil.rightPage(10)).isEqualTo(9);
+	}
+
+	/**
+	 * ページ数が null のとき
+	 * @throws Exception
+	 */
+	@Test
+	public void ページ数がnull() throws Exception {
+		assertThat(PageUtil.rightPage(null)).isEqualTo(0);
+	}
 
 }
