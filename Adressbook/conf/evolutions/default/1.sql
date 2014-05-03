06e6345038646a6818910d66d7bff19332b69961
hiroo takaku <slgerkamp@gmail.com>
2014-05-03 14:34:06 +0900

    9.2.5 AddressBookクラスの作成

---
+++ Adressbook/conf/evolutions/default/1.sql
@@ -0,0 +1,16 @@
+# --- !Ups
+
+create table addressbook (
+  id                        bigint(20) not null auto_increment,
+  name                      varchar(50),
+  age                       int,
+  tel                       varchar(50),
+  address                   varchar(128),
+  primary key (id))
+;
+
+
+
+# --- !Downs
+
+drop table if exists addressbook;
