0d22e6374be724bd1d407cf66b1a13a862089ebd
hiroo takaku <slgerkamp@gmail.com>
2014-05-04 17:34:30 +0900

    8.4.3 ログイン用認証ユーザーのEbeanモデル作成とDB設定まわり

---
+++ auth/app/models/Admin.java
@@ -0,0 +1,50 @@
+package models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import play.data.validation.Constraints.Required;
+import play.db.ebean.Model;
+
+@Entity
+@Table(name="admins", uniqueConstraints = @UniqueConstraint(columnNames = "username"))
+public class Admin extends Model{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	public Long id;
+	
+	@Required
+	public String username;
+	
+	@Required
+	public String password;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
---
+++ auth/conf/evolutions/default/1.sql
@@ -0,0 +1,28 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table admins (
+  id                        bigint not null,
+  username                  varchar(255),
+  password                  varchar(255),
+  constraint uq_admins_1 unique (username),
+  constraint pk_admins primary key (id))
+;
+
+create sequence admins_seq;
+
+
+
+
+# --- !Downs
+
+SET REFERENTIAL_INTEGRITY FALSE;
+
+drop table if exists admins;
+
+SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists admins_seq;
+
