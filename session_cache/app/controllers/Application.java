c2eff44257c630e7220262a08df4ca0a464157e5
hiroo takaku <slgerkamp@gmail.com>
2014-05-04 14:28:57 +0900

    8.1.1 セッション 実装

--- session_cache/app/controllers/Application.java
+++ session_cache/app/controllers/Application.java
@@ -1,20 +1,45 @@
 package controllers;
 
+import static play.data.Form.form;
+
+import play.data.Form;
 import play.libs.F.Option;
 import play.mvc.Controller;
 import play.mvc.Result;
 import utils.OptionUtil;
-import views.html.index;
+import views.html.*;
 
 public class Application extends Controller {
 
+    // 様々な入力形式の入力フォームに対応する入出力インタフェース
+	public static class SampleForm {
+		public String input;
+	}
+	
     public static Result index() {
-    	// セッションを追加する
-    	// アクセスしてきたユーザにセッションを返すため
-    	session("sessionText", "セッションに格納しました");
+    	Form<SampleForm> f = form(SampleForm.class);
     	// セッションから値の取得
-    	Option<String> ops = OptionUtil.applyWithString(session("sessionText"));
-        return ok(index.render(ops.getOrElse("セッションがない")));
+    	Option<String> sessionOpt = OptionUtil.applyWithString(session("sessionText"));
+        return ok(index.render(sessionOpt.getOrElse("セッションがない"), f));
+    }
+
+    public static Result send(){
+
+    	// 入力値の取得
+    	// アクセスしてきたユーザにセッションを返すため
+    	Form<SampleForm> f = new Form(SampleForm.class).bindFromRequest();
+    	// バリデーション
+    	if(!f.hasErrors()){
+	    	Option<String> inputOpt = OptionUtil.applyWithString(f.get().input);
+	
+	    	// こんなことは普通しませんが...
+	    	// セッションに追加する
+	    	session("sessionText", inputOpt.getOrElse("未入力"));
+	    	
+	    	return redirect(controllers.routes.Application.index());
+    	} else {
+    		return badRequest(index.render("エラー", f));
+    	}
     }
 
 }
