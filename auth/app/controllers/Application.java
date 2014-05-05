32f4e60e0221409882af537320c36291b71f2640
hiroo takaku <slgerkamp@gmail.com>
2014-05-05 14:12:07 +0900

    8.4.6  認証用にControllerを拡張

--- auth/app/controllers/Application.java
+++ auth/app/controllers/Application.java
@@ -1,19 +1,58 @@
 package controllers;
 
-import play.*;
+import static play.data.Form.form;
+
+import models.Login;
+import play.data.Form;
+import play.filters.csrf.AddCSRFToken;
+import play.filters.csrf.RequireCSRFCheck;
 import play.mvc.*;
-
 import views.html.*;
-
 public class Application extends Controller {
 
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
 
-
+    /**
+     * ログイン画面を表示する
+     * ログインフォームを設定するだけ
+     * CookieにCSRFトークンを挿入する
+     * @return
+     */
+    @AddCSRFToken
     public static Result login() {
+        return ok(login.render(form(Login.class)));
+    }
+
+    /**
+     * loginメソッド経由でアクセスされる
+     * 認証が終わったら、目的にURLにリダイレクトする
+     * RequireCSRFCheck：Session内にSCRFトークンが必要
+     * @return
+     */
+    @RequireCSRFCheck
+    public static Result authenticate() {
+        // ログインフォームのバリデーション
+    	Form<Login> loginForm = form(Login.class).bindFromRequest();
+    	if(loginForm.hasErrors()){
+    		return badRequest(login.render(loginForm));
+    	} else {
+    		session("username", loginForm.get().getUsername());
+    		String returnUrl = ctx().session().get("returnUrl");
+    		if(returnUrl == null 
+    				|| returnUrl.equals("")
+    				|| returnUrl.equals(routes.Application.login().absoluteURL(request()))){
+    			returnUrl = routes.Application.index().url();
+    		}
+    		return redirect(returnUrl);
+    	}
+    }
+
+
+    public static Result logout() {
         return TODO;
     }
+
 
 }
